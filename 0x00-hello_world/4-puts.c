1e86730a1ced42d151ebfa8b8f6de30a68cd61b3
odinaka <odinakanwobodo28@gmail.com>
2022-06-02 18:23:31 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * main - prints string
+ * with a new line
+ * return : 0
+ */
+int main(void)
+{
+	putchar('"');
+	puts("Prgramming is like building a multilingual puzzle");
+	return (0);
+}
+
