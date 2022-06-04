732bc8a9f171f8b68cedba15f9e20665d10b766d
odinaka <odinakanwobodo28@gmail.com>
2022-06-03 19:41:16 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter 'q')
-			putchar(letter);
-	}
-	
-	putchar('\n');
+        char letter;
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                if (letter != 'e' && letter 'q')
+                        putchar(letter);
+        }
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
