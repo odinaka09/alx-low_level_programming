5e6f9fb9f14fa8bc48be9f592d4ea10fd92bec2d
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 16:12:13 -0700

    jnjv

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char a = 'a';
 
@@ -14,5 +14,5 @@ int main(void)
 		a++;
 	}
 	_putchar('\n');
-	return (0);
+	return (void);
 }
