273c825293c55ac13fa1b9dc0d346dbf7f18dae6
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 16:07:41 -0700

    njnc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
