2dede75845ec8b794bd276381cc149613387be28
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 16:42:43 -0700

    gvgv

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 	char a = 'a';
-	int i = 0;
+	char i = 0;
 
 	while (i < 10)
 	{
@@ -16,7 +16,7 @@ void print_alphabet_x10(void)
 			_putchar(a);
 			a++;
 		}
-		i++;
 		_putchar('\n');
+		i++;
 	}
 }
