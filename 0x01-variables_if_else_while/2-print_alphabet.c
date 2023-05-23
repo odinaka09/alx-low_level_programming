5e31631abad1af5a0cef77c3c45f29be6047643d
odinaka <odinakanwobodo28@gmail.com>
2023-05-22 17:02:10 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 97;
+	while (a < 123)
+		putchar(a);
+	putchar(\n);
+	return (0);
+}
