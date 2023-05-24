54312a4e45bd0c7f84202b0ea39c79890a81bdb5
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 18:40:34 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - prints addition of value
+ * @m: the characteer to add
+ * @n: the character to add
+ * Return: Always 0 (Success)
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
