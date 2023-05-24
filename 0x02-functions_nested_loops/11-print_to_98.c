eda946153ec9617b6bb65991497f3ef159b2af20
odinaka <odinakanwobodo28@gmail.com>
2023-05-24 15:39:29 -0700

    f

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers fron n to 98
+ * @n: number
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		printf("%d", n);
+		printf(",");
+		printf(" ";)
+		n++;
+	}
+	while (n > 98)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		printf("%d", n);
+		printf(",");
+		printf(" ";)
+		n--;
+	}
+}
