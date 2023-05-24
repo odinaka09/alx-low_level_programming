f3e30a2543b0b266e86746d323b93cbeef82b1e8
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 18:14:55 -0700

    fjv

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	char mins;
+	char hrs = 0;
+
+	while (i < 24)
+	{
+		mins = 0;
+
+		while (mins >= 0 && mins <= 59)
+		{
+			_putchar('0' + (hrs / 10));
+			_putchar('0' + (hrs % 10));
+			_putchar(':');
+			_putchar('0' + (mins / 10));
+			_putchar('0' + (mins % 10));
+			_putchar('\n');
+			mins++;
+		}
+		hrs++;
+	}
+}
