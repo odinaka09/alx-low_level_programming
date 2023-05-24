d45a84c12a92e6f5393efa141ac0381677f35023
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 17:52:56 -0700

    fj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: the character to check
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int b;
+
+	b = n % 10;
+	return (b);
+}
