02c47b95d19b78ff526813f5d6bab4f1ee86ae40
odinaka <odinakanwobodo28@gmail.com>
2023-05-23 18:28:30 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - prints 9 timestable
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	char r;
+	char c = 0;
+
+	while (c < 10)
+	{
+		r = 0;
+
+		while (r >= 0 && r < 10)
+		{
+			_putchar(r * 9);
+			_putchar(',');
+			_putchar(' ');
+			r++;
+		}
+		c++;
+		_putchar('\n')
+	}
+}
