64c6c1cfaf137a600f40d50868ad210f99d1515e
odinaka <odinakanwobodo28@gmail.com>
2023-05-26 20:32:24 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - checks if c is upper
+ * @c: int to check
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int b;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		b = 1;
+	}
+	else
+		b = 0;
+	return (b);
+}
