1db4e28a7f13e7775bbbb6d2eedfc48fcfaf3031
odinaka <odinakanwobodo28@gmail.com>
2023-05-29 07:07:03 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	char c;
+	int b;
+
+	b = 0;
+	while (b < 10)
+	{
+		c = '0'
+		while (c < '15')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		b++;
+	}
+	_putchar('\n';)
+}
