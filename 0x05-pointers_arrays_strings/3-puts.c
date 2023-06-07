418b4d181104aafcba918079960c40b3eee3f7be
odinaka09 <odinakanwobodo28@gmail.com>
2023-06-07 18:23:11 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: character to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int 1;
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
