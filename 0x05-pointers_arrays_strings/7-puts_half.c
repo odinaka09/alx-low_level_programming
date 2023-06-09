fe48d66dc53f524bea68f9c4d119a817711edb46
odinaka09 <odinakanwobodo28@gmail.com>
2023-06-09 23:24:53 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,11 @@ void puts_half(char *str)
 	while (str[len] != 0)
 		len++;
 
-	if (len % 2 == 1)
+	if (len % 2 == 0)
 		i = len / 2;
 	else
-		i = (len - 1) / 2;
-	
-	i++;
+		i = (len + 1) / 2;
+
 	while (i < len)
 	{
 		_putchar(str[i]);
