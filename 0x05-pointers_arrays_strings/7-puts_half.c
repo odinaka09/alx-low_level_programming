b2b7813e5d4df765f6afb40a38689592b45c493a
odinaka09 <odinakanwobodo28@gmail.com>
2023-06-09 23:02:38 +0000

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: character to print
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+
+	while (str[len] != 0)
+		len++;
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
